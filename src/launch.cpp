6259b26fa5d84e4a860134cbd079eae3d6bd0c73
CollioTV <colliotv.epitech@gmail.eu>
2015-09-25 23:32:37 +0200

    Rule Inheritence done

--- src/launch.cpp
+++ src/launch.cpp
@@ -6,23 +6,30 @@
 #include "BNF/grammar.hpp"
 
 
-class GrammarTest : public cBNF::Grammar<GrammarTest,
-        cBNF::Rule<makePPString("entry"), makePPString("&[ #test[ :id[ second ] ] eof ]")>,
-        cBNF::Rule<makePPString("second"), makePPString("?[ &[ id :var[ id ]  ?[ num ] ] ]")>
-                > {
+class GrammarTest : public cBNF::Grammar<GrammarTest> {
 public:
     GrammarTest()
-            : Grammar({
+            : Grammar(), hooks({
             {"test", [](GrammarTest&, cBNF::Node& context, cBNF::varTable& table) -> bool {
               std::cout << "called ? {" << table["id"]->value() << "}" << std::endl;
               return true;
             }}
-    }, "entry") { }
+    }), grammar("entry") { }
+
+public:
+    std::map<std::string, std::function<bool(GrammarTest&, cBNF::Node&, cBNF::varTable&)> >
+            hooks;
+    cBNF::GrammarTable<GrammarTest,
+                       cBNF::Rule<makePPString("entry"), makePPString("&[ #test[ :id[ _second2 ] ] eof ]")>,
+                       cBNF::Rule<makePPString("_second2"), makePPString("?[ &[ id :var[ id ]  +[ '0'->'9' ] ] ]")>
+        >   grammar;
+
 };
 
 int main() {
     GrammarTest grammar;
 
+    int status = 0;
     std::cout << std::boolalpha << (bool)grammar.parse(" tttt ttt 123 ") << std::endl;
     return 0;
 }
